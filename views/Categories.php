0926c270e41eef6dea70ca567a38fba86d31b5da
unknown <stoykostanchev@gmail.com>
2014-07-06 23:28:30 +0300

    Added bootstrap and some css

--- views/Categories.php
+++ views/Categories.php
@@ -3,17 +3,40 @@
 namespace Convertonet\Views{
     class Categories extends SimpleView{
         public function getContent(){
+            $html = $this->getCategoriesHtml();
+            $html .= $this->getGamesHtml();
+            
+            return $html;
+        }
+        private function getCategoriesHtml(){            
             $categories = $this->vm->getCategories();
+            
+            $html  = '<div class="container-fluid categories">';
+   
+            foreach($categories as $cat){
+                $html .= '<div class="col-xs-3 category-wrapper">'
+                    . '<button class="category">'. ucfirst($cat) . ' Games'.'</button>'                        
+                        .'</div>';
+            }
+            $html .= '</div>';
+            return $html;
+        }
+        private function getGamesHtml(){
             $games = $this->vm->getAllGames();
             
-            $html = '';
+            $html  = '<div class="container-fluid games">';
             
-            foreach($categories as $cat){
-                $html .= '<span>' . $cat . '</span> | ';
+            foreach($games as $game){                
+                $html .= '<div class="col-xs-4 game-wrapper">'.
+                        '<div class="game">'.
+                        '<a class="head" href=#> '.$game->game_name.' </a>'.
+                        '<div class="body">'.
+                        '<img src=""></img>'
+                       .'</div>' 
+                       .'</div>' .
+                        '</div>';
             }
-            foreach($games as $game){
-                $html .= '<div>' . $game->game_name. '</div>';
-            }
+            $html .= '</div>';
             return $html;
         }
     }
--- views/View.php
+++ views/View.php
@@ -2,15 +2,40 @@
 
 namespace Convertonet\Views{
     abstract class View{                
-        public static function wrap($content){
-            $html = '<html>' .
+        private static $css = array(
+            'utils/external/bootstrap-3.2.0-dist/css/bootstrap.css',
+            'utils/external/bootstrap-3.2.0-dist/css/bootstrap-theme.css',
+            'resources/styles/categories.css'            
+        );
+        private static $js = array();
+        
+        public function wrap($content){
+            $html = '<!DOCTYPE html>' .
+                    '<html lang="en">' .
                     '<head>' . self::getHeadHtml() . '</head>'.
-                    '<body>' . $content . '</body>' . 
+                    '<body class="body">' . $content . '</body>' . 
                     '</html>'; 
             return $html;
         }
-        public static function getHeadHtml(){
-            return '<title>'. 'Controvet' . '</title>';
+        public static function getHeadHtml(){            
+            return '<title>'. 'Controvet' . '</title>'
+                    . self::getCssIncludes() 
+                    . self::getJsIncludes();
+        }
+        private static function getJsIncludes(){
+            $html = '';
+            
+            foreach(self::$js as $url){
+                $html .= "<script type='text/javascript' src='{$url}'>";
+            }
+        }
+        private static function getCssIncludes(){
+            $html = '';
+            
+            foreach(self::$css as $url){
+                $html .= "<link rel='stylesheet' type='text/css' href='{$url}'>";
+            }
+            return $html;
         }
         abstract public function display();
     }
